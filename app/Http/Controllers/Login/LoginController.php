c64a95f463bd12f880631451b8519de72320389f
Pugpaprika21 <pugpaprika21@gmail.com>
2022-09-27 05:26:15 -0700

    e

--- app/Http/Controllers/Login/LoginController.php
+++ app/Http/Controllers/Login/LoginController.php
@@ -15,22 +15,42 @@ class LoginController extends Controller
      */
     public function login(Request $request): object
     {
-        $query = RegisterModel::where('username', $request->username)
-            ->where('password', $request->password)
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $query = RegisterModel::where('username', $username)
+            ->where('password', $password)
             ->where(fn (object $query): object => $query->orWhere('role', 'admin'))
             ->get();
 
         if ($query) {
 
-            return response()->json([
-                'status' => ResponseStatus::OK->value,
-                'username' => $request->username,
-                'password' => $request->password
-            ]);
+            if ($this->validationLogin($username, $password)->auth()) {
+
+                return response()->json([
+                    'status' => ResponseStatus::OK->value,
+                    'username' => $username,
+                    'password' => $password
+                ]);
+            }
         }
 
         return response()->json([
             'status' => ResponseStatus::SERVER_ERROR->value,
         ]);
+    }
+    /**
+     * @param string $username
+     * @param string $password
+     * @return object
+     */
+    public function validationLogin(string $username, string $password): object
+    {
+        return $this;
+    }
+
+    public function auth(): bool
+    {
+        return true;
     }
 }
